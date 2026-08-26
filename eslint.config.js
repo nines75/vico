bcb88b27367996149a27775599190ebeb4218c20
nines <127805872+nines75@users.noreply.github.com>
2026-08-27 01:01:17 +0900

    refactor: setup WXT

---
+++ eslint.config.js
@@ -0,0 +1,113 @@
+import js from "@eslint/js";
+import ts from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
+import prettier from "eslint-config-prettier/flat";
+import { importX } from "eslint-plugin-import-x";
+import unicorn from "eslint-plugin-unicorn";
+import regex from "eslint-plugin-regexp";
+
+const isCi = process.env.CI === "true";
+
+export default defineConfig(
+  globalIgnores([".output/", ".wxt/", "eslint.config.js"]),
+
+  // https://typescript-eslint.io/getting-started
+  js.configs.recommended,
+  ts.configs.strictTypeChecked,
+  ts.configs.stylisticTypeChecked,
+
+  // https://github.com/un-ts/eslint-plugin-import-x
+  importX.flatConfigs.typescript,
+
+  // https://github.com/sindresorhus/eslint-plugin-unicorn
+  unicorn.configs.recommended,
+
+  // https://github.com/ota-meshi/eslint-plugin-regexp
+  regex.configs.recommended,
+
+  {
+    files: ["**/*.{ts,tsx}"],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    rules: {
+      // -------------------------------------------------------------------------------------------
+      // error => warn
+      // -------------------------------------------------------------------------------------------
+
+      "no-empty": "warn",
+      "@typescript-eslint/no-empty-function": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/require-await": "warn",
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+
+      // -------------------------------------------------------------------------------------------
+      // change options
+      // -------------------------------------------------------------------------------------------
+
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+          ignoreRestSiblings: true,
+        },
+      ],
+      "@typescript-eslint/restrict-template-expressions": [
+        "error",
+        {
+          allowAny: false,
+          allowBoolean: false,
+          allowNullish: false,
+          allowRegExp: false,
+        },
+      ],
+      "unicorn/filename-case": [
+        "error",
+        {
+          cases: {
+            kebabCase: true,
+            pascalCase: true,
+          },
+        },
+      ],
+
+      // -------------------------------------------------------------------------------------------
+      // disable
+      // -------------------------------------------------------------------------------------------
+
+      "unicorn/name-replacements": "off",
+      "unicorn/no-null": "off",
+      "unicorn/no-break-in-nested-loop": "off",
+      "unicorn/require-array-sort-compare": "off",
+      "unicorn/max-nested-calls": "off",
+      "unicorn/prefer-else-if": "off",
+      "unicorn/isolated-functions": "off",
+      "unicorn/consistent-boolean-name": "off",
+
+      // -------------------------------------------------------------------------------------------
+      // enable
+      // -------------------------------------------------------------------------------------------
+
+      eqeqeq: "error",
+      "no-param-reassign": "error",
+      "no-shadow": ["error", { allow: ["_"] }],
+      "@typescript-eslint/consistent-type-imports": "warn",
+      "@typescript-eslint/require-array-sort-compare": "error",
+      "@typescript-eslint/switch-exhaustiveness-check": "error",
+      "@typescript-eslint/strict-boolean-expressions": [
+        "error",
+        {
+          allowString: false,
+          allowNumber: false,
+          allowNullableObject: false,
+        },
+      ],
+      "import-x/no-cycle": ["error", { maxDepth: isCi ? Infinity : 1 }],
+    },
+  },
+
+  prettier,
+);
---
+++ wxt.config.ts
@@ -0,0 +1,47 @@
+import { defineConfig } from "wxt";
+
+export default defineConfig({
+  modules: ["@wxt-dev/auto-icons"],
+  imports: false,
+  zip: {
+    artifactTemplate: "firefox.xpi",
+  },
+  vite: ({ mode }) => {
+    const isProduction = mode === "production";
+
+    return {
+      build: {
+        ...(isProduction && {
+          license: {
+            fileName: `license-${crypto.randomUUID()}.json`,
+          },
+        }),
+      },
+    };
+  },
+  manifest: ({ mode }) => {
+    const isDevelopment = mode === "development";
+
+    return {
+      permissions: ["notifications", "storage"],
+      commands: {
+        "open-settings": {
+          description: "open settings",
+          suggested_key: isDevelopment ? { default: "Alt+O" } : {},
+        },
+        _execute_browser_action: {
+          description: "open popup",
+          suggested_key: isDevelopment ? { default: "Alt+K" } : {},
+        },
+      },
+      browser_specific_settings: {
+        gecko: {
+          id: "{ac7945b9-252e-43ae-b047-23d50acf61ab}",
+          data_collection_permissions: {
+            required: ["none"],
+          },
+        },
+      },
+    };
+  },
+});
